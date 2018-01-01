b886e2f0260376a486e9b78c5cb3fb9ff7cb5dc1
KhanhNguyen1511 <mr.m8223.knnd@gmail.com>
2018-01-01 13:24:40 +0700

    Tạo nhánh Function và thêm file text

--- app/src/main/java/com/example/mrm82/motelfinderapp/UserLogin.java
+++ app/src/main/java/com/example/mrm82/motelfinderapp/UserLogin.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -71,7 +72,9 @@ public class UserLogin extends AppCompatActivity {
                             FirebaseUser user =mAuth.getCurrentUser();
 
                         }else {
-                            Toast.makeText(UserLogin.this, "Log in fail", Toast.LENGTH_SHORT).show();
+                            Log.w("error", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(UserLogin.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
